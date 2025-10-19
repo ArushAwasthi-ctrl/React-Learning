909b9d39826afb8f26702079b2a9130f8a5658d0
Arush <arushawasthi3@gmail.com>
2025-10-20 00:10:32 +0530

    create Pages

--- Blog Project/Blog Project/src/components/index.js
+++ Blog Project/Blog Project/src/components/index.js
@@ -8,4 +8,7 @@ import PostCard from './PostCard'
 import Select from './Select'
 import Input from './Input'
 import RTE from "./RTE";
-export { Header, Footer, LogOutBtn, Container, Logo , Button ,PostCard , Select , Input , RTE};
+import PostForm from "./post-form/PostForm";
+import SignUp from "../components/SignUp";
+import Login from "./Login";
+export { Header, Footer, LogOutBtn, Container, Logo , Button ,PostCard , Select , Input , RTE , PostForm , SignUp , Login};
